d07406d4ed5ccecde116a0ee720b79d9a65c163f
baanant <antti.paananen@alumni.aalto.fi>
2015-02-15 16:04:39 +0200

    Facade added.

---
+++ DesignPatterns/Facade.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns
+{
+    public class Customer : ICustomer
+    {
+        public Customer(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+    }
+
+    public interface ICustomer
+    {
+        string Name { get; set; }
+    }
+
+    class Bank
+    {
+        public bool HasSufficientFunds(ICustomer cust)
+        {
+            Console.WriteLine("Check funds for " + cust.Name + ".");
+            return true;
+        }
+    }
+
+    class Credit
+    {
+        public bool HasSufficientCredit(ICustomer cust)
+        {
+            Console.WriteLine("Check credit for " + cust.Name + "." );
+            return true;
+        }
+    }
+    public class MortageFacade
+    {
+        private Bank bank = new Bank();
+        private Credit credit = new Credit();
+
+        public bool IsEligible(ICustomer cust, int amount)
+        {
+            Console.WriteLine("Customer " + cust.Name + " is applying for loan worth " + amount.ToString() + " euro.");
+            bool eligible = true;
+            if(!bank.HasSufficientFunds(cust)) eligible = false;
+            if(!credit.HasSufficientCredit(cust)) eligible = false;
+            return eligible;
+        }
+    }
+
+
+}
--- Tester/TesterProgram.cs
+++ Tester/TesterProgram.cs
@@ -25,6 +25,15 @@ namespace Tester
             Console.WriteLine("Let's test our singleton class:");
             if (s1 == s2) Console.WriteLine("If you see this text, it means that the singleton works and the two instances s1 and s2 are the same.");
             Console.WriteLine("-------------------------------------------------------------------------------------------------------------------");
+
+            ///This is a test for Singleton design pattern.
+            Console.WriteLine("Let's test our facade class:");
+            MortageFacade f = new MortageFacade();
+            Customer c = new Customer("Pablo Picasso");
+            int amount = 250000;
+            bool eligible = f.IsEligible(c, amount);
+            Console.WriteLine("Mortage worth " + amount.ToString() + " is " + (eligible ? "approved" : "rejected") + " for customer "  +  c.Name + ".");
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------");
             Console.ReadLine();
         }
     }

b74e67e20de62c0926a86312789942cd1d5cd439
baanant <antti.paananen@alumni.aalto.fi>
2015-02-15 15:05:45 +0200

    Implemented factory and singleton design patterns.

---
+++ DesignPatterns/Factory.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public static class Factory
+    {
+        public static Position Get(int typeId)
+        {
+            switch (typeId)
+            {
+                case 1:     return new QAManager();
+                case 2:     return new Developer();
+                case 3:     return new Salesman();
+            }
+            return null;
+        }
+    }
+
+
+    public abstract class Position
+    {
+        public abstract int TypeID { get; }
+        public string Department { get; set; }
+        public abstract string PositionType { get; }
+        public abstract string PositionName { get; }
+    }
+
+
+    internal class QAManager:Position
+    {
+        public override int TypeID { get { return 1; } }
+
+        public override string PositionType { get { return "Manager"; } }
+
+        public override string PositionName { get { return "Quality Assurance Manager"; } }
+
+    }
+
+    internal class Developer:Position
+    {
+        public override int TypeID { get { return 2; } }
+
+        public override string PositionType { get { return "Official"; } }
+
+        public override string PositionName { get { return "Software Developer"; } }
+    }
+
+    internal class Salesman : Position
+    {
+
+        public override int TypeID { get { return 3; } }
+
+        public override string PositionType { get { return "Official"; } }
+
+        public override string PositionName { get { return "Sales assistant"; } }
+    }
+
+
+
+
+}
---
+++ Tester/TesterProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DesignPatterns;
+
+namespace Tester
+{
+    class TesterProgram
+    {
+        static void Main(string[] args)
+        {
+            ///This is a test for Factory design pattern.
+            Console.WriteLine("Let's test our factory class:");
+            for (int i = 1; i <= 3; i++)
+            {
+                var position = Factory.Get(i);
+                Console.WriteLine("The position with type id: "
+                    + position.TypeID.ToString() + ", is "
+                    + position.PositionType.ToString() + " position. The name of the position is "
+                    + position.PositionName.ToString() + ".");
+            }
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------");
+
+            ///This is a test for Singleton design pattern.
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+            Console.WriteLine("Let's test our singleton class:");
+            if (s1 == s2) Console.WriteLine("If you see this text, it means that the singleton works and the two instances s1 and s2 are the same.");
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------");
+            Console.ReadLine();
+        }
+    }
+}
